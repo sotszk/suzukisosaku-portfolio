246e3ac5fd71b0dd9533c27e046b8c09308f6ef2
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-02-03 18:54:05 +0900

    astro image の integration をコンフィグに追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import image from '@astrojs/image';
+
+// https://astro.build/config
 export default defineConfig({
   server: {
     port: 4000,
   },
+  integrations: [
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
 });
