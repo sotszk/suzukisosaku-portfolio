14fb15270b118a270d2ba4b3724ca636f463d999
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-02-03 18:51:17 +0900

    Setup eslint with astro and ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,19 @@
 module.exports = {
-  extends: ['plugin:astro/recommended'],
+  extends: [
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:astro/base',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
   overrides: [
     {
       files: ['*.astro'],
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_BASE_URL: string;
+  // 環境変数が増えたら追加する
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
