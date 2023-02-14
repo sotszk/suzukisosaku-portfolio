88c62b68bcf7adf06baa5eec565b2a1d79f09e2f
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-02-14 17:09:28 +0900

    Add deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://github.com/sotszk.github.io',
+  base: '/suzukisosaku-portfolio',
   server: {
     port: 4000,
   },
